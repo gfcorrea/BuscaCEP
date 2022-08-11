3c180bcc69dbea211352458fd6350a17b0b568c5
Gabriel <gfcorrea01@gmail.com>
2022-08-10 22:25:45 -0300

    Trabalhando com ExecutorService

--- app/src/main/java/com/gfcorrea/buscacep/MainActivity.java
+++ app/src/main/java/com/gfcorrea/buscacep/MainActivity.java
@@ -1,14 +1,24 @@
 package com.gfcorrea.buscacep;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+
+
 import android.view.View;
 import android.widget.Toast;
 
 import com.gfcorrea.buscacep.api.ViaCepAPI;
 import com.gfcorrea.buscacep.databinding.ActivityMainBinding;
 import com.gfcorrea.buscacep.model.CepReturnModel;
+import com.gfcorrea.buscacep.viewmodel.CepViewModel;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -19,6 +29,7 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
+    ExecutorService executor = Executors.newSingleThreadExecutor();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,41 +37,88 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        CepViewModel cepViewModel = new ViewModelProvider(this).get(CepViewModel.class);
+
+
+        final Observer<String> logradouroObserver = new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                binding.lblLogradouro.setText("Logradouro: " + s);
+            }
+        };
+
+        final Observer<String> cidadeObserver = new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                binding.lblCidade.setText("Cidade: " + s);
+            }
+        };
+
+        final Observer<String> ufObserver = new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                binding.lblUF.setText("UF: " + s);
+            }
+        };
+
+        final Observer<String> bairroObserver = new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                binding.lblBairro.setText("Bairro: " + s);
+            }
+        };
+
+        final Observer<String> ibgeObserver = new Observer<String>() {
+            @Override
+            public void onChanged(String s) {
+                binding.lblIBGE.setText("Nº IBGE: " + s);
+            }
+        };
+
+        cepViewModel.getLogradouro().observe(this, logradouroObserver);
+        cepViewModel.getLocalidade().observe(this, cidadeObserver);
+        cepViewModel.getBairro().observe(this, bairroObserver);
+        cepViewModel.getNibge().observe(this, ibgeObserver);
+        cepViewModel.getUf().observe(this, ufObserver);
+
 
         binding.btnPesquisar.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl("https://viacep.com.br/")
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
 
-                ViaCepAPI apiService = retrofit.create(ViaCepAPI.class);
+                executor.execute(() -> {
 
-                Call<CepReturnModel> call = apiService.getCepInformation(binding.txtCEP.getText().toString());
+                    Retrofit retrofit = new Retrofit.Builder()
+                            .baseUrl("https://viacep.com.br/")
+                            .addConverterFactory(GsonConverterFactory.create())
+                            .build();
 
-                call.enqueue(new Callback<CepReturnModel>() {
-                    @Override
-                    public void onResponse(Call<CepReturnModel> call, Response<CepReturnModel> response) {
-                        if (!response.isSuccessful()) {
-                            Toast.makeText(MainActivity.this, "ERROR onResponde: " + response.code(), Toast.LENGTH_LONG).show();
-                        } else {
-                            CepReturnModel cep = response.body();
+                    ViaCepAPI apiService = retrofit.create(ViaCepAPI.class);
 
-                            binding.lblCidade.setText("Cidade: " + cep.getLocalidade());
-                            binding.lblUF.setText("UF: " + cep.getUf());
-                            binding.lblLogradouro.setText("Logradouro: " + cep.getLogradouro());
-                            binding.lblBairro.setText("Bairro: " + cep.getBairro());
-                            binding.lblIBGE.setText("Nº IBGE: " + cep.getIbge());
+                    Call<CepReturnModel> call = apiService.getCepInformation(binding.txtCEP.getText().toString());
 
+                    call.enqueue(new Callback<CepReturnModel>() {
+                        @Override
+                        public void onResponse(Call<CepReturnModel> call, Response<CepReturnModel> response) {
+                            if (!response.isSuccessful()) {
+                                Toast.makeText(MainActivity.this, "ERROR onResponde: " + response.code(), Toast.LENGTH_LONG).show();
+                            } else {
+                                CepReturnModel cep = response.body();
+                                cepViewModel.getLocalidade().setValue(cep.getLocalidade());
+                                cepViewModel.getBairro().setValue(cep.getBairro());
+                                cepViewModel.getNibge().setValue(cep.getIbge());
+                                cepViewModel.getLogradouro().setValue(cep.getLogradouro());
+                                cepViewModel.getUf().setValue(cep.getUf());
+                            }
                         }
-                    }
 
-                    @Override
-                    public void onFailure(Call<CepReturnModel> call, Throwable t) {
-                        t.printStackTrace();
-                        Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
-                    }
+                        @Override
+                        public void onFailure(Call<CepReturnModel> call, Throwable t) {
+                            t.printStackTrace();
+                            Toast.makeText(MainActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+                        }
+
+                    });
 
                 });
 
---
+++ app/src/main/java/com/gfcorrea/buscacep/viewmodel/CepViewModel.java
@@ -0,0 +1,41 @@
+package com.gfcorrea.buscacep.viewmodel;
+
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class CepViewModel extends ViewModel {
+
+    private MutableLiveData<String> localidade;
+    private MutableLiveData<String> uf;
+    private MutableLiveData<String> logradouro;
+    private MutableLiveData<String> bairro;
+    private MutableLiveData<String> nibge;
+
+    public CepViewModel() {
+    }
+
+    public MutableLiveData<String> getLocalidade() {
+        if(localidade == null){ localidade = new MutableLiveData<String>(); }
+        return localidade;
+    }
+
+    public MutableLiveData<String> getUf() {
+        if(uf == null){ uf = new MutableLiveData<String>(); }
+        return uf;
+    }
+
+    public MutableLiveData<String> getLogradouro() {
+        if(logradouro == null){ logradouro = new MutableLiveData<String>(); }
+        return logradouro;
+    }
+
+    public MutableLiveData<String> getBairro() {
+        if(bairro == null){ bairro = new MutableLiveData<String>(); }
+        return bairro;
+    }
+
+    public MutableLiveData<String> getNibge() {
+        if(nibge == null){ nibge = new MutableLiveData<String>(); }
+        return nibge;
+    }
+}
