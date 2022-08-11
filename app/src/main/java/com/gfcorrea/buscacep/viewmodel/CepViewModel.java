730af736c2864004f2e1b30034690f83c98a1355
Gabriel <gfcorrea01@gmail.com>
2022-08-10 22:35:58 -0300

    Refatorando Código.

--- app/src/main/java/com/gfcorrea/buscacep/viewmodel/CepViewModel.java
+++ app/src/main/java/com/gfcorrea/buscacep/viewmodel/CepViewModel.java
@@ -8,38 +8,8 @@ import com.gfcorrea.buscacep.model.CepReturnModel;
 public class CepViewModel extends ViewModel {
 
     private MutableLiveData<CepReturnModel> cep;
-    private MutableLiveData<String> localidade;
-    private MutableLiveData<String> uf;
-    private MutableLiveData<String> logradouro;
-    private MutableLiveData<String> bairro;
-    private MutableLiveData<String> nibge;
 
     public CepViewModel() {
-    }
-
-    public MutableLiveData<String> getLocalidade() {
-        if(localidade == null){ localidade = new MutableLiveData<String>(); }
-        return localidade;
-    }
-
-    public MutableLiveData<String> getUf() {
-        if(uf == null){ uf = new MutableLiveData<String>(); }
-        return uf;
-    }
-
-    public MutableLiveData<String> getLogradouro() {
-        if(logradouro == null){ logradouro = new MutableLiveData<String>(); }
-        return logradouro;
-    }
-
-    public MutableLiveData<String> getBairro() {
-        if(bairro == null){ bairro = new MutableLiveData<String>(); }
-        return bairro;
-    }
-
-    public MutableLiveData<String> getNibge() {
-        if(nibge == null){ nibge = new MutableLiveData<String>(); }
-        return nibge;
     }
 
     public MutableLiveData<CepReturnModel> getCep() {
